8a3535303563f47b7d321018f5ebd884881c1d78
J'aime Dangui <jaime.dangui@ayokai.com>
2023-06-28 10:34:56 +0100

    first commit

---
+++ Refresh/async-prom.js
@@ -0,0 +1,6 @@
+setTimeout(()=> {
+    console.log('Le temps est écoulé!');
+}, 2000)
+
+console.log('Salut salut');
+console.log('Bonjour');
---
+++ Refresh/intro.js
@@ -0,0 +1,66 @@
+
+// let Uname = "Jaime";
+// let pass = "Mypass"
+
+// function Amusestoi(userName, PassWord){
+//     return (
+//         'Name is '
+//         +userName+
+//         ', and Password is '
+//         +PassWord
+//     );
+// }
+
+// console.log(Amusestoi(Uname, pass));
+
+// const person = {
+//     Name : "Jaime",
+//     Pass : "Mypass",
+//     greet () {
+//         console.log("Salut je m'appelle " + this.Name);
+//     }
+// }
+
+// //structuration d'objet
+// const printName = ({Name}) => {
+//     console.log(Name);
+// }
+
+// printName(person);
+
+// const {Name} = person;
+// console.log(Name);
+
+// person.greet();
+
+// const hobbies = ['Sports', 'Cooking'];
+
+// const [Sp, Act] = hobbies;
+// console.log(Sp);
+
+//structuration de tableau
+// const [hobby] = hobbies;
+// console.log(hobby);
+
+// console.log(hobbies.map( hobby => {
+//     return ('hobbie: ' + hobby)
+// }));
+// console.log("----------------------------------");
+// console.log(hobbies);
+
+// hobbies.push('Programme')
+// console.log(hobbies);
+
+// const cpHobb = hobbies.slice() // ou 
+
+//un spread
+// const cpHobb = [...hobbies]
+// console.log(cpHobb);
+
+// opérateur de repos REST
+
+// const tab = (...args) => {
+//     return args
+// }
+
+// console.log(tab(1,2,4,3));
