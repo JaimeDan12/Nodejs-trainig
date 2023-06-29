b9d1cf882c4e34fca212e58b29281c641a83ff43
J'aime Dangui <jaime.dangui@ayokai.com>
2023-06-29 09:33:57 +0100

    adding pm2 configs

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    script: './Bases/index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      key  : './kkb-key.key',
+      user : 'ubuntu',
+      host : '195.15.195.132',
+      ref  : 'origin/dev',
+      repo : 'https://github.com/JaimeDan12/Nodejs-trainig.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
