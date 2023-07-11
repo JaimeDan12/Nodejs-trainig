ee4a8c8d96143909c42db1854d14f43f1ffd797e
J'aime Dangui <jaime.dangui@ayokai.com>
2023-07-11 11:14:18 +0100

    Adding routes fliter and 404 page

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const bodyPrser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(bodyPrser.urlencoded({extended:true,}));
+app.use(bodyParser.urlencoded({extended:true,}));
 
-app.use(adminRoutes); //pas besoin de l'appeler avec () comme une fonction
+app.use('/admin', adminRoutes); //pas besoin de l'appeler avec () comme une fonction
 app.use(shopRoutes);
 
+app.use((req, res, next) =>{
+    res.status(404).send('<h1>Page not found</h1>'); 
+})
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<h1>Products page</h1><form action="/product " method="POST"><input type=text name ="tittle"><button type=submit>Add product</button></form>')
+    res.send('<h1>Products page</h1><form action="/admin/product " method="POST"><input type=text name ="tittle"><button type=submit>Add product</button></form>')
 })
 
 router.post('/product', (req, res, next) =>{
