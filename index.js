21cf24fd9c25ea674e2742ed1e060a928b324482
J'aime Dangui <jaime.dangui@ayokai.com>
2023-08-08 10:59:19 +0100

    Working with dynamic content and adding template engines

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const rootDir = require('./util/path')
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true,}));
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.use('/admin', adminRoutes); //pas besoin de l'appeler avec () comme une fonction
+app.use('/admin', adminData.routes); //pas besoin de l'appeler avec () comme une fonction
 app.use(shopRoutes);
 
 app.use((req, res, next) =>{
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const rootDir = require('../util/path')
+const adminData = require('./admin');
+const { log } = require('console');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 }); 
- 
-module.exports = router; 
+
+module.exports = router;   
