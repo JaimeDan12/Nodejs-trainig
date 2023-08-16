1108f5107849490eb88e78d20e6256f6edac31af
J'aime Dangui <jaime.dangui@ayokai.com>
2023-08-16 14:49:39 +0100

    finish pug

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended:true,}));
 app.use(express.static(path.join(__dirname, 'public'))); 
 
@@ -15,7 +18,7 @@ app.use('/admin', adminData.routes); //pas besoin de l'appeler avec () comme une
 app.use(shopRoutes);
 
 app.use((req, res, next) =>{
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html')); 
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.pug')); 
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,23 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const products = []; 
+const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-})
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+});
 
-router.post('/add-product', (req, res, next) =>{
-    products.push({ title: req.body.title }); 
-    // console.log(products); 
-    res.redirect('/'); 
-}) 
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
 
-exports.routes = router; 
-exports.products = products; 
+exports.routes = router;
+exports.products = products;
